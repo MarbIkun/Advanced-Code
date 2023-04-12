4a3480f74c45f62da05dc43b6b9aa064d56d52cc
MRBING <1390603258@qq.com>
2023-04-12 17:33:38 +0800

    方法重写

---
+++ day01-code/src/com/mrbing/extendsdemo2/Test.java
@@ -0,0 +1,12 @@
+package com.mrbing.extendsdemo2;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student("坤坤",18);
+        System.out.println(s1.toString());
+
+        String s2 = "123";
+        System.out.println(s2);
+
+    }
+}
