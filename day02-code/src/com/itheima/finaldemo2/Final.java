b4285b2872b4936787a65297d7ef6b94278d0932
MRBING <1390603258@qq.com>
2023-04-13 11:22:08 +0800

    final

---
+++ day02-code/src/com/itheima/finaldemo2/Final.java
@@ -0,0 +1,8 @@
+package com.itheima.finaldemo2;
+
+public class Final {
+    static final String SCHOOL_NAME = "白马程序员";
+    static final String NAME = "坤坤";
+    static final int AGE = 250;
+    static final String DI_ZHI = "西安";
+}
---
+++ day02-code/src/com/itheima/finaldemo2/Test.java
@@ -0,0 +1,15 @@
+package com.itheima.finaldemo2;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Final.SCHOOL_NAME);
+        System.out.println(Final.NAME);
+        System.out.println(Final.AGE);
+        System.out.println(Final.DI_ZHI);
+        System.out.println(Final.SCHOOL_NAME);
+        System.out.println(Final.NAME);
+        System.out.println(Final.AGE);
+        System.out.println(Final.DI_ZHI);
+        System.out.println(Final.NAME);
+    }
+}
